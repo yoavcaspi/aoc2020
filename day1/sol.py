965c2009f08001cdabe1aee8605bab48a8fa57e8
Yoav Caspi <yoavcaspi@gmail.com>
2020-11-30 22:01:11 +0200

    preparation for day 1

---
+++ day1/sol.py
@@ -0,0 +1,25 @@
+import argparse
+from typing import List
+
+
+def sol(data: List[str]) -> int:
+    ...
+
+
+def get_input(filename: str) -> List[str]:
+    with open(filename) as f:
+        lines = f.readlines()
+    return lines
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', default='input.txt')
+    args = parser.parse_args()
+    data = get_input(args.filename)
+    print(sol(data))
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
